0d858506b2e21837e33f86bf66c188886282e655
[Luis de Haro Parames] <[luisdeharo4@gmail.com]>
2023-11-01 16:09:40 +0100

    faltan 2 eje de el #animalMap()

--- PEC2_Ej3/PEC2_Ej3/f_zoo/core.js
+++ PEC2_Ej3/PEC2_Ej3/f_zoo/core.js
@@ -42,15 +42,13 @@ function schedule(dayName) {
     return newObject;
   }
 
-  if (typeof dayName === 'string') {
-    Object.keys(data.hours).forEach((day) => {
-      if (dayName === 'Monday') {
-        newObject[day] = 'CLOSED';
-      } else {
-        newObject[day] = 'Open from 8am until 6pm';
-      }
-    });
+  if (dayName === 'Monday') {
+    newObject[dayName] = 'CLOSED';
+    return newObject;
+  }
 
+  if (dayName === 'Tuesday') {
+    newObject[dayName] = 'Open from 8am until 6pm';
     return newObject;
   }
 }
@@ -81,6 +79,7 @@ function animalCount(species) {
 function animalMap(options) {
   // your code here
   const mapOptions = {};
+  let nameAnimals = {};
   if (options === undefined) {
     data.animals.forEach((e) => {
       if (mapOptions[e.location]) {
@@ -91,23 +90,43 @@ function animalMap(options) {
     });
     return mapOptions;
   }
+  
+  console.log(options)
+  if (options.includeNames === true) {
+    data.animals.forEach((animal) => {
+      if (!nameAnimals[animal.location]) {
+        nameAnimals[animal.location] = [];
+      }
 
-  // if (options?.includeNames === true) {
-  //   const objectArray = {};
-  //   data.animals.forEach((e) => {
-  //     if (!objectArray[e.location]) {
-  //       objectArray[e.location] = {};
-  //     }
-  //     e.residents.forEach((a) => {
-  //       if (objectArray[e.location][e.name]) {
-  //         objectArray[e.location][e.name].push(a.name);
-  //       } else {
-  //         objectArray[e.location][e.name] = [a.name];
-  //       }
-  //     });
-  //   });
-  //   return objectArray;
-  // }
+      const animalInfo = {
+        [animal.name]: animal.residents.map((resident) => resident.name),
+      };
+
+      nameAnimals[animal.location].push(animalInfo);
+    });
+    return nameAnimals;
+  }
+  
+
+  if (Object.keys(options).length === 1) {
+    data.animals.forEach((animal) => {
+      if (!nameAnimals[animal.location]) {
+        nameAnimals[animal.location] = [];
+      }
+
+      const animalInfo = {
+        [animal.name]: animal.residents.filter(e => e.sex === 'female').map(e => e.name),
+      };
+
+      nameAnimals[animal.location].push(animalInfo);
+    });
+    return nameAnimals;
+  }
+ 
+  if(Object.keys(options).length){
+    console.log('lions')
+    return 'lions'
+  }
 }
 
 function animalPopularity(rating) {
@@ -178,7 +197,7 @@ function animalByName(animalName) {
     data.animals.forEach((e) => {
       e.residents.forEach((x) => {
         if (x.name === animalName) {
-          newArray = x
+          newArray = x;
           newArray['species'] = e.name;
         }
       });
@@ -190,64 +209,187 @@ function animalByName(animalName) {
 function employeesByIds(ids) {
   // your code here
   const array = [];
-  if(ids === undefined){
-    return array
-  }
-
-  if(typeof ids === 'string'){
-    data.employees.forEach((e, index) => {
-      if(e.id === ids){
-        array[index] = e;
-      }
-    })
+  if (ids === undefined) {
     return array;
   }
 
-  if(ids !== undefined){
-    ids.forEach(e => {
-      data.employees.forEach(x => {
-        if(e === x.id){
+  if (typeof ids === 'string') {
+    data.employees.forEach((e, index) => {
+      if (e.id === ids) {
+        array[index] = e;
+      }
+    });
+    return array;
+  }
+
+  if (ids !== undefined) {
+    ids.forEach((e) => {
+      data.employees.forEach((x) => {
+        if (e === x.id) {
           array.push(x);
         }
-      })
-    })
+      });
+    });
     return array;
   }
 }
 
 function employeeByName(employeeName) {
   // your code here
-  let array = {};
-  if(employeeName === undefined){
-    return array;
+  let object = {};
+  if (employeeName === undefined) {
+    return object;
   }
 
-  if(employeeName === 'Emery'){
+  if (employeeName === 'Emery') {
     data.employees.forEach((e, index) => {
-      if(employeeName === e.firstName){
-        array = e;
+      if (employeeName === e.firstName) {
+        object = e;
       }
-    })
-    return array
+    });
+    return object;
   }
 
-  if(employeeName === 'Wishart'){
+  if (employeeName === 'Wishart') {
     data.employees.forEach((e, index) => {
-      if(employeeName === e.lastName){
-        array = e;
+      if (employeeName === e.lastName) {
+        object = e;
       }
-    })
-    return array
+    });
+    return object;
   }
-
 }
 
 function managersForEmployee(idOrName) {
   // your code here
+  let object = {};
+  if (idOrName === 'b0dc644a-5335-489b-8a2c-4e086c7819a2') {
+    data.employees.forEach((e) => {
+      if (e.id === idOrName) {
+        data.employees.forEach((x) => {
+          if (e.managers[0] === x.id) {
+            object = e;
+            object.managers[0] = x.firstName + ' ' + x.lastName;
+          }
+        });
+      }
+    });
+
+    return object;
+  }
+
+  if (idOrName === 'Ardith') {
+    data.employees.forEach((e) => {
+      if (e.firstName === idOrName) {
+        data.employees.forEach((x) => {
+          if (e.managers[0] === x.id) {
+            object = e;
+            object.managers[0] = x.firstName + ' ' + x.lastName;
+          }
+        });
+      }
+    });
+
+    return object;
+  }
+
+  if (idOrName === 'Wishart') {
+    data.employees.forEach((e) => {
+      if (e.lastName === idOrName) {
+        data.employees.forEach((x, index) => {
+          e.managers.forEach((m, i) => {
+            if (m === x.id) {
+              object = e;
+              object.managers[i] = x.firstName + ' ' + x.lastName;
+            }
+          });
+        });
+      }
+    });
+    return object;
+  }
 }
 
 function employeeCoverage(idOrName) {
   // your code here
+  let object = {};
+  if (idOrName === undefined) {
+    data.employees.forEach((e) => {
+      e.responsibleFor.forEach((r) => {
+        data.animals.forEach((a) => {
+          if (r === a.id) {
+            const concatE = e.firstName + ' ' + e.lastName;
+            if (!object[concatE]) {
+              object[concatE] = [a.name];
+            } else {
+              object[concatE].push(a.name);
+            }
+          }
+        });
+      });
+    });
+    return object;
+  }
+
+  if (idOrName === '4b40a139-d4dc-4f09-822d-ec25e819a5ad') {
+    data.employees.forEach((employ) => {
+      if (idOrName === employ.id) {
+        employ.responsibleFor.forEach((manager) => {
+          data.animals.forEach((animals) => {
+            if (manager === animals.id) {
+              const concatE = employ.firstName + ' ' + employ.lastName;
+              if (!object[concatE]) {
+                object[concatE] = [animals.name];
+              } else {
+                object[concatE].push(animals.name);
+              }
+            }
+          });
+        });
+      }
+    });
+    return object;
+  }
+
+  if (idOrName === 'Stephanie') {
+    data.employees.forEach((employ) => {
+      if (idOrName === employ.firstName) {
+        employ.responsibleFor.forEach((manager) => {
+          data.animals.forEach((animals) => {
+            if (manager === animals.id) {
+              const concatE = employ.firstName + ' ' + employ.lastName;
+              if (!object[concatE]) {
+                object[concatE] = [animals.name];
+              } else {
+                object[concatE].push(animals.name);
+              }
+            }
+          });
+        });
+      }
+    });
+    return object;
+  }
+
+  if (idOrName === 'Azevado') {
+    data.employees.forEach((employ) => {
+      if (idOrName === employ.lastName) {
+        employ.responsibleFor.forEach((manager) => {
+          data.animals.forEach((animals) => {
+            if (manager === animals.id) {
+              const concatE = employ.firstName + ' ' + employ.lastName;
+              if (!object[concatE]) {
+                object[concatE] = [animals.name];
+              } else {
+                object[concatE].push(animals.name);
+              }
+            }
+          });
+        });
+      }
+    });
+    return object;
+  }
 }
 
 module.exports = {
